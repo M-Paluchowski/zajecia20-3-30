8333e4497d5abf905e0919f2a3f767c387d1d398
Marcin Paluchowski <marcin.paluchowski@cognifide.com>
2020-09-17 18:46:09 +0200

    Zajecia 20-3-31

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.form.UserForm;
 import com.example.demo.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,5 +35,29 @@ public class UserController {
     @GetMapping("/{id}")
     User get(@PathVariable Long id) {
         return new User(id, "Tomasz", "Kowalski");
+    }
+
+    @PostMapping
+    ResponseEntity<User> create(@RequestBody UserForm userForm) {
+        //przekazanie do serwisu i utworzenie uzytkownika
+        User newUser = new User(new Random().nextLong(), userForm.getFirstName(), userForm.getLastName());
+        URI uri = UriComponentsBuilder.fromPath("/api/users/{id}")
+                .build(newUser.getId());
+
+        return ResponseEntity.created(uri).body(newUser);
+    }
+
+    @PutMapping("/{id}")
+    void update(@PathVariable Long id, @RequestBody UserForm userForm) {
+        //edycja
+        //zapytać bazę danych czy dany użytkownik istnieje
+        //jeżeli istnieje to wyciągnać go z bazy danych zupdatować mu dane i zapisać do bazy
+        //jeżeli nie istnieje to stworzyć nowego uzytkownika
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    void delete(@PathVariable Long id) {
+        //usunięcie
     }
 }
---
+++ src/main/java/com/example/demo/form/UserForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.form;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserForm {
+
+    private String firstName;
+    private String lastName;
+}
